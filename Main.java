9ad39cc049ad5f8f3782f1bad743fe794d4d542f
mharada337 <150187121+mharada337@users.noreply.github.com>
2023-12-11 01:39:28 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -75,7 +75,9 @@ public class Main extends JPanel implements KeyListener{
             }
         }; //jumps
         if (c == 'j'){}; // attacks
-        if (c == 'k'){}; // switches between dead and alive
+        if (c == 'k') {
+            world.player.deadState();
+        };
         if (c == 'm'){}; // opens map
         if (c == 'p'){}; // opens the menus; change later to Esc if possible
     } // keyPressed()
